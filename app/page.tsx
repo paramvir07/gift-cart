fb9da9d0167143630f80e74a3e2e3428ae520f1e
Shreyas Dutt <151562739+ShreyasDutt@users.noreply.github.com>
2026-07-08 22:59:10 -0700

    feat: add NotFound page, CallToAction, FaqsSection components, and update page layout

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,5 @@
+import { CallToAction } from "@/components/shared/landing/cta"
+import { FaqsSection } from "@/components/shared/landing/faqs-section"
 import Hero01 from "@/components/shared/landing/hero-01"
 
 
@@ -5,6 +7,10 @@ export default function Page() {
   return (
     <div>
       <Hero01/>
+      <div className="mb-32">
+      <CallToAction/>
+      </div>
+      <FaqsSection/>
     </div>
   )
 }
